870c675b62d162bf8e01fcbffb2c11c21380dcbc
g3ncu <g3ncucsgo@gmail.com>
2026-07-30 01:07:58 +0300

    Create route.js

---
+++ app/api/auth/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const provider = searchParams.get('provider');
+
+  if (provider === 'github') {
+    const clientId = process.env.GITHUB_CLIENT_ID;
+    const redirectUri = `https://www.cstweak.com.tr/api/auth/callback`;
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo,user`;
+
+    return NextResponse.redirect(githubAuthUrl);
+  }
+
+  return NextResponse.json({ error: 'Provider not supported' }, { status: 400 });
+}
